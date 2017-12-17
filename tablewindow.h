92a81886d9a8e21ef62f84ca2a629f28d65a540d
Zheka001 <aidarkinzhenya@mail.ru>
2017-12-17 09:41:02 +0300

    v1.0.5

---
+++ tablewindow.h
@@ -0,0 +1,12 @@
+#ifndef TABLEWINDOW_H
+#define TABLEWINDOW_H
+
+class TableWindow : public QAbstractTableModel
+{
+public:
+    explicit TableWindow(QObject* parent = 0);
+
+    int
+}
+
+#endif // TABLEWINDOW_H
